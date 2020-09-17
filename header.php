57974b5429c6b0e995868b9683c617f03c9a00e6
Yannik Seitz <yannikseitz@outlook.de>
2020-09-17 17:43:10 +0200

    logout

--- header.php
+++ header.php
@@ -15,25 +15,31 @@
 <body>
     <header>
         <nav class="nav-header-main">
-            <a href="#">
-                <img class="header-logo" src="img/22314.png" alt="logo">
+            <a class="nav-header-logo-a" href="index.php">
+                <img class="nav-header-logo" src="img/22314.png" alt="logo">
             </a>
             <ul class="nav-header-list">
                 <li><a class="nav-header-ref" href="index.php">Home</a></li>
-                <li><a class="nav-header-ref" href="#">Portfolio</a></li>
-                <li><a class="nav-header-ref" href="#">About Me</a></li>
-                <li><a class="nav-header-ref" href="#">Contact</a></li>
+                <li><a class="nav-header-ref" href="#">Stuff</a></li>
+                <li><a class="nav-header-ref" href="#">More Stuff Me</a></li>
+                <li><a class="nav-header-ref" href="#">All the Stuff</a></li>
             </ul>
-            <div class="header-login">
-                <form action="includes/login.inc.php" method="post">
-                    <input class="nav-header-input" type="text" name="mailuid" placeholder="Username/Email...">
-                    <input class="nav-header-input" type="password" name="pwd" placeholder="Password...">
-                    <button class="nav-header-button" type="submit" name="login-submit">Login</button>
-                </form>
-                <a class="nav-header-ref" href="signup.php">Signup</a>
-                <form action="includes/logout.inc.php" method="post">
-                    <button class="nav-header-button" type="submit" name="logout-submit">Logout</button>
-                </form>
+            <div class="nav-header-login">
+                <?php
+                    if (isset($_SESSION['userId'])) {
+                        echo '  <form action="includes/logout.inc.php" method="post">
+                                    <button class="nav-header-button" type="submit" name="logout-submit">Logout</button>
+                                </form>';
+                    }
+                    else {
+                        echo '  <form action="includes/login.inc.php" method="post">
+                                    <input class="nav-header-input" type="text" name="mailuid" placeholder="Username/Email...">
+                                    <input class="nav-header-input" type="password" name="pwd" placeholder="Password...">
+                                    <button class="nav-header-button" type="submit" name="login-submit">Login</button>
+                                </form>
+                                <a class="nav-header-ref" href="signup.php">Signup</a>';
+                    }
+                ?>
             </div>
         </nav>
     </header>
